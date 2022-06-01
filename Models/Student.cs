eb486009135309d12e7dcc1daab0d03e4c9cd762
jaswanth2712 <104197700+jaswanth2712@users.noreply.github.com>
2022-06-01 11:01:22 +0530

    Add project files.

---
+++ Models/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        [Column("FirstName")]
+        public string FirstMidName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return LastName + ", " + FirstMidName;
+            }
+        }
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
